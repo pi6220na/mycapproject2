2c36bbbb17f809760a324560653dd161b0ddb83a
My name <22032408+pi6220na@users.noreply.github.com>
2018-01-19 10:00:41 -0600

    created fileio module. removed direct file manipulation code from datastore.py

--- datastore.py
+++ datastore.py
@@ -1,56 +1,11 @@
 
-import os
+import os, fileio
 from book import Book
-
-DATA_DIR = 'data'
-BOOKS_FILE_NAME = os.path.join(DATA_DIR, 'wishlist.txt')
-COUNTER_FILE_NAME = os.path.join(DATA_DIR, 'counter.txt')
 
 separator = '^^^'  # a string probably not in any valid data relating to a book
 
 book_list = []
 counter = 0
-
-def setup():
-    ''' Read book info from file, if file exists. '''
-
-    global counter
-
-    try :
-        with open(BOOKS_FILE_NAME) as f:
-            data = f.read()
-            make_book_list(data)
-    except FileNotFoundError:
-        # First time program has run. Assume no books.
-        pass
-
-
-    try:
-        with open(COUNTER_FILE_NAME) as f:
-            try:
-                counter = int(f.read())
-            except:
-                counter = 0
-    except:
-        counter = len(book_list)
-
-
-def shutdown():
-    '''Save all data to a file - one for books, one for the current counter value, for persistent storage'''
-
-    output_data = make_output_data()
-
-    # Create data directory
-    try:
-        os.mkdir(DATA_DIR)
-    except FileExistsError:
-        pass # Ignore - if directory exists, don't need to do anything. 
-
-    with open(BOOKS_FILE_NAME, 'w') as f:
-        f.write(output_data)
-
-    with open(COUNTER_FILE_NAME, 'w') as f:
-        f.write(str(counter))
 
 
 def get_books(**kwargs):
